17c839d2151b2bb7c46a0962804360a7aa042c53
saipavan2128 <59191203+saipavan2128@users.noreply.github.com>
2020-07-18 11:33:06 +0530

    Create dark.py

---
+++ dark.py
@@ -0,0 +1,25 @@
+dark is an accurate facebook account information gathering, all sensitive information can be easily gathered even though the target converts all of its privacy to (only me), Sensitive information about residence, date of birth, occupation, phone number and email address.
+
+
+
+# [ Installation ]Kali linux
+```
+$ apt update && apt upgrade
+$ apt install git python2
+$ git clone https://github.com/pavan-daram/dark.git
+$ cd dark
+```
+# [ Installation ]Mac-OSX (Sierra-High Sierra)-by Knightsec
+```
+$ brew install python2
+$ git clone https://github.com/pavan-daram/dark.git
+$ cd dark
+```
+# [ Setup ]
+```
+$ pip2 install -r requirements.txt        
+```
+# [ Running ]
+```
+$ python2 dark.py      
+```
